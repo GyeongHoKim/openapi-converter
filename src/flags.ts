16b174adf89279dd8a7264e9b29c879869557818
GyeongHo Kim <gyeongho.dev@proton.me>
2025-12-10 00:06:14 +0900

    feat: flag definition using zod schema
    - there is two flags: output and provider

---
+++ src/flags.ts
@@ -0,0 +1,17 @@
+import * as z from "zod";
+import { ValidationError } from "./errors/validation.js";
+
+const flags = z.object({
+	output: z.string().optional(),
+	provider: z.string().optional(),
+});
+
+export type Flags = z.infer<typeof flags>;
+
+export const validateFlags = (input: unknown): Flags => {
+	const result = flags.safeParse(input);
+	if (result.error) {
+		throw new ValidationError(z.prettifyError(result.error));
+	}
+	return result.data;
+};
