dce6b8d4d64d89ca61fd2a88572f8378f9d7b020
GyeongHo Kim <gyeongho.dev@proton.me>
2025-12-10 00:23:55 +0900

    feat: converter definitions
    - converter type definition
    - converter factory for each providers

---
+++ src/converter/converter.ts
@@ -0,0 +1,9 @@
+import type { FileHandler } from "../infrastructure/io.js";
+
+export abstract class Converter {
+	constructor(
+		protected readonly outputPath: string,
+		protected readonly fileHandler: FileHandler,
+	) {}
+	abstract convert(input: string): never;
+}
---
+++ src/converter/factory.ts
@@ -0,0 +1,26 @@
+import { BadRequestError } from "../errors/bad-request.js";
+import type { Flags } from "../flags.js";
+import { FSFileHandler } from "../infrastructure/file-handler.js";
+import { APIDogConverter } from "./apidog-converter.js";
+import { BrunoConverter } from "./bruno-converter.js";
+import type { Converter } from "./converter.js";
+import { PostmanConverter } from "./postman-converter.js";
+
+export const createConverter = (flags: Flags): Converter => {
+	const provider = flags.provider?.toLowerCase();
+	const outputPath = flags?.output ?? "./openapi.json";
+	const fileHandler = new FSFileHandler();
+	if (!provider) {
+		return new PostmanConverter(outputPath, fileHandler);
+	}
+	switch (provider) {
+		case "postman":
+			return new PostmanConverter(outputPath, fileHandler);
+		case "apidog":
+			return new APIDogConverter(outputPath, fileHandler);
+		case "bruno":
+			return new BrunoConverter(outputPath, fileHandler);
+		default:
+			throw new BadRequestError(`Not Supported: ${provider}`);
+	}
+};
